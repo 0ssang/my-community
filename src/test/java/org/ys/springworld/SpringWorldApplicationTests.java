72f152db97aab0b64e03a74bf2a8dffd55fdc198
조영상 <51320595+0ssang@users.noreply.github.com>
2024-05-07 23:56:02 +0900

    postgreSQL로 변경

---
+++ src/test/java/org/ys/springworld/SpringWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ys.springworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
